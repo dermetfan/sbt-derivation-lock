5e6304f1168080e740043b90857bcdef6477e291
Robin Stumm <serverkorken@gmail.com>
2022-06-13 21:50:17 +0200

    forward version

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     overlay = final: prev: {
       sbt = prev.sbt.overrideAttrs (oldAttrs: {
         passthru = oldAttrs.passthru or {} // {
+          inherit (prev.sbt) version;
           mkDerivation = prev.callPackage ./sbt-derivation.nix { inherit sbt-derivation; };
         };
       });
