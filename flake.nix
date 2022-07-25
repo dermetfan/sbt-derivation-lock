12f3526b867c659a3253ca4e25039680b7e77533
Robin Stumm <robin.stumm@iohk.io>
2022-07-25 09:56:57 +0200

    move overlay to separate file
    existed but was not used before

--- flake.nix
+++ flake.nix
@@ -1,14 +1,7 @@
 {
   inputs.sbt-derivation.url = github:zaninime/sbt-derivation;
 
-  outputs = { sbt-derivation, ... }: {
-    overlay = final: prev: {
-      sbt = prev.sbt.overrideAttrs (oldAttrs: {
-        passthru = oldAttrs.passthru or {} // {
-          inherit (prev.sbt) version;
-          mkDerivation = prev.callPackage ./sbt-derivation.nix { inherit sbt-derivation; };
-        };
-      });
-    };
+  outputs = inputs: {
+    overlay = import ./overlay.nix inputs;
   };
 }
--- overlay.nix
+++ overlay.nix
@@ -3,6 +3,7 @@ inputs:
 final: prev: {
   sbt = prev.sbt.overrideAttrs (oldAttrs: {
     passthru = oldAttrs.passthru or {} // {
+      inherit (prev.sbt) version;
       mkDerivation = prev.callPackage ./sbt-derivation.nix { inherit (inputs) sbt-derivation; };
     };
   });
