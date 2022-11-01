b56c6e618b212c1b8abc78aedeef391086e528e1
Robert Schardt <robert.schardt@iohk.io>
2022-11-01 14:19:03 +0100

    Overwrite configurephase and define preConfigure phase.

--- sbt-derivation.nix
+++ sbt-derivation.nix
@@ -13,6 +13,13 @@
 let
   lockedSbt = callPackage ./sbt.nix { inherit lockFile; };
   callMkDerivation = callPackage "${sbt-derivation}/lib/sbt-derivation.nix";
+
+  sbtEnvSetupCmds = ''
+    export SBT_DEPS=$(mktemp -d)
+    export SBT_OPTS="-Dsbt.global.base=$SBT_DEPS/project/.sbtboot -Dsbt.boot.directory=$SBT_DEPS/project/.boot -Dsbt.ivy.home=$SBT_DEPS/project/.ivy $SBT_OPTS"
+    export COURSIER_CACHE=$SBT_DEPS/project/.coursier
+    mkdir -p $SBT_DEPS/project/{.sbtboot,.boot,.ivy,.coursier}
+  '';
 in
 
 (callMkDerivation { sbt = lockedSbt; } (args // {
@@ -36,14 +43,24 @@ in
   # no longer needed
   deps = null;
 
-  # explicitly overwrite the `postConfigure` phase, otherwise it
+
+  preConfigure = ''
+    ${sbtEnvSetupCmds}
+
+    # SBT expects a "local" prefix to each organization for plugins
+    for repo in ${lockedSbt.mavenRepo}/sbt-plugin-releases/*; do
+      ln -s $repo $SBT_DEPS/project/.ivy/local''${repo##*/}
+    done
+  '';
+
+  # explicitly overwrite the `configurePhase` phase, otherwise it
   # references the now null `deps` derivation.
   configurePhase = ''
     ${args.configurePhase or ""}
-    mkdir -p .nix/ivy
-    # SBT expects a "local" prefix to each organization for plugins
-    for repo in ${lockedSbt.mavenRepo}/sbt-plugin-releases/*; do
-      ln -s $repo .nix/ivy/local''${repo##*/}
-    done
+    runHook preConfigure
+
+    ${oldAttrs.passthru.dependencies.extractor} $SBT_DEPS
+
+    runHook postConfigure
   '';
 })
